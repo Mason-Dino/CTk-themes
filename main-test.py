edd474390ba979adeb84d6e3b7789bddcfca9115
LegosAndStuff <masonnorberg@midco.net>
2024-07-03 10:44:45 -0500

    just messing around with the theme

--- main-test.py
+++ main-test.py
@@ -3,7 +3,7 @@ import tkinter.messagebox
 import customtkinter
 
 customtkinter.set_appearance_mode("System")  # Modes: "System" (standard), "Dark", "Light"
-customtkinter.set_default_color_theme("themes/red.json")  # Themes: "blue" (standard), "green", "dark-blue"
+customtkinter.set_default_color_theme("themes/dark-blue.json")  # Themes: "blue" (standard), "green", "dark-blue"
 
 
 class App(customtkinter.CTk):
